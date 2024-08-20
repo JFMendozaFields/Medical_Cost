7bda45d79319ecacf2f65f6a65c5e680d99f2ccf
JFMendozaFields <jfmendozafields@gmail.com>
2024-08-20 12:56:34 -0700

    updated data reporting format, plotted distributions

--- Medical_Insurance_costs/analyze.py
+++ Medical_Insurance_costs/analyze.py
@@ -18,37 +18,47 @@ null_data = df[df.isnull().any(axis=1)]
 #print(null_data) empty results which is a good sign
 
 # checks percentage differences between males and females
-female_vs_male_counts = round(df['sex'].value_counts('female'), 2)
-print(f'Percentage between males and females',female_vs_male_counts)
+female_vs_male_counts = round(df['sex'].value_counts(normalize=True) * 100, 2)
+print(f'Percentage between males and females:', female_vs_male_counts)
 
-smokers_vs_nonsmokers_count = round(df['smoker'].value_counts('no'), 1)
+#calculates bmi 
+total_bmi = df['bmi'].sum()
+average_bmi = df['bmi'].mean()
+print(f'Average BMI: {average_bmi:.2f}')
+#calculates percent of smokers vs non-smokers
+smokers_vs_nonsmokers_count = round(df['smoker'].value_counts(normalize=True) * 100, 2)
 print(f'Percentage between smokers and non-smokers:',smokers_vs_nonsmokers_count)
 
 # calculating total costs and average costs
-total_charges = 0
-for charge in df['charges']:
-    total_charges += charge
-total_charges = round(total_charges, 2)
-print(f'total costs:', total_charges)
-average_costs = round(total_charges / len(df['charges']), 2)
-print(f'total costs:',average_costs)
-
+total_charges = df['charges'].sum()
+average_costs = df['charges'].mean()
+print(f'Total costs: {total_charges:.2f}')
+print(f'Average costs: {average_costs:.2f}')
 # Calculates percentage of records by region, pretty similar across the boardd with a slight larger influence in the southeast region
-counts_by_region = round(df['region'].value_counts('northeast'), 2)
-print(f'Customers per region:',counts_by_region)
+counts_by_region = df['region'].value_counts(normalize=True) * 100
+print(f'Customers per region: ', counts_by_region)
 
 
 #calculating average insurance customer's age
-total_ages = 0
-for age in df.age:
-    total_ages += age
-#print(total_ages)
-average_age = round(total_ages / len(df.age))
+
+average_age = round(df['age'].mean())
 print(f'Average customer age:',average_age)
 #describes the aggregates of different columns based on mean, standard deviation, minumum and maximum values as well as their points in-between
 #print(df.describe())
 
 #print(df.smoker)
 
-unpivoted_data = df.groupby(['region', 'sex', 'smoker'].mean().reset_index())
-print(unpivoted_data)
+#Distribution of charges by smoker status
+sns.histplot(data=df, x='charges', hue='smoker', multiple='stack', bins=30)
+plt.title('Distribution of Charges based on Smoking status')
+plt.xlabel('Charges')
+plt.ylabel('Frequency')
+plt.show()
+
+plt.clf()
+# Average charges based on region
+sns.boxplot(data=df, x='region', y='charges')
+plt.title('Distribution of Average cost based on region')
+plt.xlabel('Regional origin')
+plt.ylabel('Charges per Region')
+plt.show()
